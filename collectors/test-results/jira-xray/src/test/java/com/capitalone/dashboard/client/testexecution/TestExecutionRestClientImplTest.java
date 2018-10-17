cb9117d0659e173b299e728af7ea689ac196b514
Praveen-chaganti <chagantipraveen5g3@gmail.com>
2018-10-17 11:59:20 -0400

    Unit tests for TestRun and TestExec

--- collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testexecution/TestExecutionRestClientImplTest.java
+++ collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testexecution/TestExecutionRestClientImplTest.java
@@ -1,34 +1,38 @@
 package com.capitalone.dashboard.client.testexecution;
 
-import com.atlassian.httpclient.api.HttpClient;
-import com.atlassian.jira.rest.client.internal.async.AbstractAsynchronousRestClient;
 import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
+import com.atlassian.util.concurrent.Effect;
 import com.atlassian.util.concurrent.Promise;
-import com.capitalone.dashboard.client.JiraXRayRestClientImpl;
-import com.capitalone.dashboard.client.JiraXRayRestClientSupplier;
 import com.capitalone.dashboard.client.api.domain.TestExecution;
 import com.capitalone.dashboard.client.api.domain.TestRun;
+import com.capitalone.dashboard.client.core.json.TestArrayJsonParser;
+import com.google.common.base.Function;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.gson.JsonParser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.net.URI;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TestExecutionRestClientImpl.class)
 public class TestExecutionRestClientImplTest {
-    @Mock
-    private AbstractAsynchronousRestClient abstractAsynchronousRestClient;
-    @Mock
-    private JiraXRayRestClientImpl restClient;
+
     @Mock
     private TestExecution testExecution;
     @Mock
     private DisposableHttpClient httpClient;
 
-    private final JiraXRayRestClientSupplier restClientSupplier=new JiraXRayRestClientSupplier();
 
 
 
@@ -38,26 +42,88 @@ public class TestExecutionRestClientImplTest {
 
     }
     @Test
-    public void TestExecutionRestClientImplGetTests() throws Exception{
+    public void getTests() throws Exception{
         testExecution = new TestExecution(URI.create(""), "EME-4644", 1977l);
-        TestExecution.Test test = new TestExecution.Test(URI.create(""),"EA-3403",28775L,1,TestRun.Status.PASS);
-        TestExecutionRestClientImpl mock = Mockito.spy(new TestExecutionRestClientImpl(URI.create(""),httpClient));
+        TestExecution.Test test =  new TestExecution.Test(URI.create(""),"EA-3403",28775L,1,TestRun.Status.PASS);
+      //  AbstractAsynchronousRestClient mockAbstractClass = mock(AbstractAsynchronousRestClient.class);
+        TestExecutionRestClientImpl mock = PowerMockito.spy(new TestExecutionRestClientImpl(URI.create(""),httpClient));
+        final  TestArrayJsonParser testsParser=new TestArrayJsonParser();
+        Promise p =new Promise() {
+            @Override
+            public Object claim() {
+                return test;
+            }
 
-        //Iterable<TestExecution.Test> test = restClient.getTestExecutionClient().getTests(testExecution).claim();
+            @Override
+            public Promise done(Effect effect) {
+                return null;
+            }
 
-        try {
-            Iterable<TestExecution.Test> result = mock.getTests(testExecution).claim();
-            Mockito.when(mock.getTests(testExecution).claim()).thenReturn((Iterable<TestExecution.Test>) test);
-            Assert.assertNotNull(result);
+            @Override
+            public Promise fail(Effect effect) {
+                return null;
+            }
 
+            @Override
+            public Promise then(FutureCallback futureCallback) {
+                return null;
+            }
 
+            @Override
+            public Promise map(Function function) {
+                return null;
+            }
 
-        }catch(Exception e){
+            @Override
+            public Promise flatMap(Function function) {
+                return null;
+            }
 
+            @Override
+            public Promise recover(Function function) {
+                return null;
+            }
+
+            @Override
+            public Promise fold(Function function, Function function1) {
+                return null;
+            }
+
+            @Override
+            public void addListener(Runnable runnable, Executor executor) {
+
+            }
+
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                return false;
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return false;
+            }
+
+            @Override
+            public boolean isDone() {
+                return false;
+            }
+
+            @Override
+            public Object get() throws InterruptedException, ExecutionException {
+                return null;
+            }
+
+            @Override
+            public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+                return null;
+            }
+        };
+        PowerMockito.doReturn(p).when(mock,"getAndParse",Matchers.any(URI.class),Matchers.any(TestArrayJsonParser.class));
+        Promise<Iterable<TestExecution.Test>> testResult= mock.getTests(testExecution);
+        System.out.println("***************"+testResult);
+        Assert.assertNotNull(testResult);
         }
-
-
-    }
 
 
 
--- collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testrun/TestRunRestClientImplTest.java
+++ collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testrun/TestRunRestClientImplTest.java
@@ -1,179 +1,132 @@
 package com.capitalone.dashboard.client.testrun;
 
-import com.atlassian.jira.rest.client.api.SearchRestClient;
 import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
-import com.capitalone.dashboard.client.api.domain.Defect;
-import com.capitalone.dashboard.client.api.domain.TestExecution;
+import com.atlassian.util.concurrent.Effect;
+import com.atlassian.util.concurrent.Promise;
 import com.capitalone.dashboard.client.api.domain.TestRun;
-import org.codehaus.jettison.json.JSONObject;
+import com.capitalone.dashboard.client.core.json.TestArrayJsonParser;
+import com.google.common.base.Function;
+import com.google.common.util.concurrent.FutureCallback;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.net.URI;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TestRunRestClientImpl.class)
 public class TestRunRestClientImplTest {
     private final String TEST_EXEC_KEY="EME-4944";
     private final String TEST_KEY="EME-1683";
     private final long TEST_ID=507571;
-    @Mock
-    private TestRunRestClientImpl testRunRestClientImpl;
 
-
-    private JSONObject jsonObject=null;
-    @Mock
-    private TestExecution testExecution;
     @Mock
     private DisposableHttpClient httpClient;
     @Mock
-    private SearchRestClient searchRestClient=null;
+    private Promise pr;
 
     @Before
     public final void init() throws Exception {
         MockitoAnnotations.initMocks(this);
-        TestRunRestClientImpl mock = Mockito.spy(new TestRunRestClientImpl(URI.create(""),httpClient));
-        jsonObject= new JSONObject("{\n" +
-                "  \"id\" : 1977,\n" +
-                "  \"status\" : \"PASS\",\n" +
-                "  \"executedBy\" : \"cxu457\",\n" +
-                "  \"startedOn\" : \"08/ago/16 10:24 AM\",\n" +
-                "  \"finishedOn\" : \"viernes 1:19 PM\",\n" +
-                "  \"assignee\" : \"\",\n" +
-                "  \"defects\" : [\n" +
-                "    {\n" +
-                "      \"id\" : 16414,\n" +
-                "      \"key\" : \"PBT-28\",\n" +
-                "      \"summary\" : \"Especificar los detalles del requisito\",\n" +
-                "      \"status\" : \"Open\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"evidences\" : [\n" +
-                "    {\n" +
-                "      \"id\" : 2,\n" +
-                "      \"fileName\" : \"2012-04-14_130335.png\",\n" +
-                "      \"fileSize\" : \"19 kB\",\n" +
-                "      \"created\" : \"Hoy 7:16 PM\",\n" +
-                "      \"author\" : \"luis.martinez\",\n" +
-                "      \"fileURL\" : \"https://sasjira.services.connectis.es/jira/plugins/servlet/raven/attachment/2/2012-04-14_130335.png\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"comment\" : \"UN COMENTARIO DE PRUEBA\",\n" +
-                "  \"steps\" : [\n" +
-                "    {\n" +
-                "      \"id\" : 7278,\n" +
-                "      \"index\" : 1,\n" +
-                "      \"step\" : {\n" +
-                "        \"raw\" : \"CR-VC-001 ¿Se ha recogido toda la información relativa a los requisitos?\",\n" +
-                "        \"rendered\" : \"<p>CR-VC-001 ¿Se ha recogido toda la información relativa a los requisitos?</p>\"\n" +
-                "      },\n" +
-                "      \"data\" : {\n" +
-                "        \"rendered\" : \"\"\n" +
-                "      },\n" +
-                "      \"result\" : {\n" +
-                "        \"raw\" : \"Indicación de todos los campos requeridos para cada requisito.\\n•\\tCódigo único e invariable.\\n•\\tNombre.\\n•\\tVersión.\\n•\\tFecha.\\n•\\tOrigen del requisito.\\n•\\tPrioridad.\\n•\\tDescripción.\\n•\\tSolución propuesta.\\n\",\n" +
-                "        \"rendered\" : \"<p>Indicación de todos los campos requeridos para cada requisito.<br/>•Código único e invariable.<br/>•Nombre.<br/>•Versión.<br/>•Fecha.<br/>•Origen del requisito.<br/>•Prioridad.<br/>•Descripción.<br/>•Solución propuesta.</p>\"\n" +
-                "      },\n" +
-                "      \"attachments\" : [ ],\n" +
-                "      \"status\" : \"PASS\",\n" +
-                "      \"comment\" : {\n" +
-                "        \"raw\" : \"OK\",\n" +
-                "        \"rendered\" : \"<p>OK</p>\"\n" +
-                "      },\n" +
-                "      \"defects\" : [ ],\n" +
-                "      \"evidences\" : [ ]\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"id\" : 7279,\n" +
-                "      \"index\" : 2,\n" +
-                "      \"step\" : {\n" +
-                "        \"raw\" : \"CR-VC-002 ¿Se han descrito los requisitos claramente?\\n¿Están definidos los requisitos o funciones en términos no ambiguos?\\n\",\n" +
-                "        \"rendered\" : \"<p>CR-VC-002 ¿Se han descrito los requisitos claramente?<br/>¿Están definidos los requisitos o funciones en términos no ambiguos?</p>\"\n" +
-                "      },\n" +
-                "      \"data\" : {\n" +
-                "        \"rendered\" : \"\"\n" +
-                "      },\n" +
-                "      \"result\" : {\n" +
-                "        \"raw\" : \"Los requisitos deben estar descritos de forma clara y comprensible para personal técnico, que no conoce el sistema, y usuarios, que no conocen el vocabulario técnico.\\nInexistencia de ambigüedades en la redacción.\\n\",\n" +
-                "        \"rendered\" : \"<p>Los requisitos deben estar descritos de forma clara y comprensible para personal técnico, que no conoce el sistema, y usuarios, que no conocen el vocabulario técnico.<br/>Inexistencia de ambigüedades en la redacción.</p>\"\n" +
-                "      },\n" +
-                "      \"attachments\" : [ ],\n" +
-                "      \"status\" : \"FAIL\",\n" +
-                "      \"comment\" : {\n" +
-                "        \"raw\" : \"Requisito incompleto\",\n" +
-                "        \"rendered\" : \"<p>Requisito incompleto</p>\"\n" +
-                "      },\n" +
-                "      \"defects\" : [\n" +
-                "        {\n" +
-                "          \"id\" : 16414,\n" +
-                "          \"key\" : \"PBT-28\",\n" +
-                "          \"summary\" : \"Especificar los detalles del requisito\",\n" +
-                "          \"status\" : \"Open\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"evidences\" : [ ]\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"id\" : 7280,\n" +
-                "      \"index\" : 3,\n" +
-                "      \"step\" : {\n" +
-                "        \"raw\" : \"CR-VC-003 ¿Son consistentes los requisitos?\",\n" +
-                "        \"rendered\" : \"<p>CR-VC-003 ¿Son consistentes los requisitos?</p>\"\n" +
-                "      },\n" +
-                "      \"data\" : {\n" +
-                "        \"rendered\" : \"\"\n" +
-                "      },\n" +
-                "      \"result\" : {\n" +
-                "        \"raw\" : \"Inexistencia de contradicciones entre requisitos.\",\n" +
-                "        \"rendered\" : \"<p>Inexistencia de contradicciones entre requisitos.</p>\"\n" +
-                "      },\n" +
-                "      \"attachments\" : [ ],\n" +
-                "      \"status\" : \"EXECUTING\",\n" +
-                "      \"comment\" : {\n" +
-                "        \"rendered\" : \"\"\n" +
-                "      },\n" +
-                "      \"defects\" : [ ],\n" +
-                "      \"evidences\" : [ ]\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}");
-        Defect defect =new Defect(URI.create(""), "", 50756L, "", "PASS");
         TestRun testRun = new TestRun(URI.create(""), "EME-1683", 507571L, TestRun.Status.PASS, null, null, "jqm884", "jqm884", null, null, null, null, null);
+        Promise pr =new Promise() {
+            @Override
+            public Object claim() {
+                return testRun;
+            }
+
+            @Override
+            public Promise done(Effect effect) {
+                return null;
+            }
+
+            @Override
+            public Promise fail(Effect effect) {
+                return null;
+            }
+
+            @Override
+            public Promise then(FutureCallback futureCallback) {
+                return null;
+            }
+
+            @Override
+            public Promise map(Function function) {
+                return null;
+            }
+
+            @Override
+            public Promise flatMap(Function function) {
+                return null;
+            }
+
+            @Override
+            public Promise recover(Function function) {
+                return null;
+            }
+
+            @Override
+            public Promise fold(Function function, Function function1) {
+                return null;
+            }
+
+            @Override
+            public void addListener(Runnable runnable, Executor executor) {
+
+            }
+
+            @Override
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                return false;
+            }
+
+            @Override
+            public boolean isCancelled() {
+                return false;
+            }
+
+            @Override
+            public boolean isDone() {
+                return false;
+            }
+
+            @Override
+            public Object get() throws InterruptedException, ExecutionException {
+                return null;
+            }
+
+            @Override
+            public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+                return null;
+            }
+        };
     }
     @Test
-    public void getTestRunsByTestExecKeyAndTestKey() {
-        TestRunRestClientImpl mock = Mockito.spy(new TestRunRestClientImpl(URI.create(""),httpClient));
-        try {
-            TestRun testruns = mock.getTestRun(TEST_EXEC_KEY, TEST_KEY).claim();
-             Mockito.when(mock.getTestRun(TEST_EXEC_KEY, TEST_KEY).claim()).thenReturn((TestRun) jsonObject.get("1977"));
-             Assert.assertEquals(testruns.getAssignee(), "jqm88");
-             Assert.assertEquals(testruns.getExecutedBy(), "jqm88");
-             Assert.assertEquals(testruns.getKey(), "EME-1683");
-        }catch (Exception e){
+    public void getTestRunsByTestExecKeyAndTestKey() throws Exception {
 
-        }
-
-
+        TestRunRestClientImpl mock = PowerMockito.spy(new TestRunRestClientImpl(URI.create(""),httpClient));
+        PowerMockito.doReturn(pr).when(mock,"getAndParse",Matchers.anyObject(),Matchers.any(TestArrayJsonParser.class));
+        Promise<TestRun> testruns = mock.getTestRun(TEST_EXEC_KEY, TEST_KEY);
+        Assert.assertNotNull(testruns);
 
     }
     @Test
-    public void  getTestRunByTestRunId(){
-        TestRunRestClientImpl mock = Mockito.spy(new TestRunRestClientImpl(URI.create(""),httpClient));
-        try {
-            TestRun testruns = mock.getTestRun(TEST_ID).claim();
-            Mockito.when(mock.getTestRun(TEST_EXEC_KEY, TEST_KEY).claim()).thenReturn((TestRun) jsonObject.get("1977"));
-            Assert.assertEquals(testruns.getKey(), "EME-1683");
-            Assert.assertEquals(testruns.getExecutedBy(), "jqm884");
-        }catch (Exception e){
-
-        }
-
-
+    public void  getTestRunByTestRunId()throws Exception{
+        TestRunRestClientImpl mock = PowerMockito.spy(new TestRunRestClientImpl(URI.create(""),httpClient));
+        PowerMockito.doReturn(pr).when(mock,"getAndParse",Matchers.any(URI.class),Matchers.any(TestArrayJsonParser.class));
+        Promise<TestRun> testruns = mock.getTestRun(TEST_ID);
+        Assert.assertNotNull(testruns);
     }
 
 }
