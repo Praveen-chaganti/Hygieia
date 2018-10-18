68a1a20431609d04a55cf61dfb379e7ab2b2d6f8
Praveen-chaganti <chagantipraveen5g3@gmail.com>
2018-10-18 12:26:47 -0400

    code CleanUp

--- collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testexecution/TestExecutionRestClientImplTest.java
+++ collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testexecution/TestExecutionRestClientImplTest.java
@@ -1,14 +1,11 @@
 package com.capitalone.dashboard.client.testexecution;
 
 import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
-import com.atlassian.util.concurrent.Effect;
 import com.atlassian.util.concurrent.Promise;
 import com.capitalone.dashboard.client.api.domain.TestExecution;
 import com.capitalone.dashboard.client.api.domain.TestRun;
 import com.capitalone.dashboard.client.core.json.TestArrayJsonParser;
-import com.google.common.base.Function;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.gson.JsonParser;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,10 +16,6 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.net.URI;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(TestExecutionRestClientImpl.class)
@@ -32,92 +25,39 @@ public class TestExecutionRestClientImplTest {
     private TestExecution testExecution;
     @Mock
     private DisposableHttpClient httpClient;
+    @Mock
+    private Promise pr;
+
 
     @Before
     public final void init() throws Exception {
         MockitoAnnotations.initMocks(this);
+        TestExecution.Test test =  new TestExecution.Test(URI.create(""),"EA-3403",28775L,1,TestRun.Status.PASS);
+        PowerMockito.when(pr.claim()).thenReturn(test);
 
     }
     @Test
     public void getTests() throws Exception{
         testExecution = new TestExecution(URI.create(""), "EME-4644", 1977l);
-        TestExecution.Test test =  new TestExecution.Test(URI.create(""),"EA-3403",28775L,1,TestRun.Status.PASS);
         TestExecutionRestClientImpl mock = PowerMockito.spy(new TestExecutionRestClientImpl(URI.create(""),httpClient));
-        Promise p =new Promise() {
-            @Override
-            public Object claim() {
-                return test;
-            }
-
-            @Override
-            public Promise done(Effect effect) {
-                return null;
-            }
-
-            @Override
-            public Promise fail(Effect effect) {
-                return null;
-            }
-
-            @Override
-            public Promise then(FutureCallback futureCallback) {
-                return null;
-            }
-
-            @Override
-            public Promise map(Function function) {
-                return null;
-            }
-
-            @Override
-            public Promise flatMap(Function function) {
-                return null;
-            }
-
-            @Override
-            public Promise recover(Function function) {
-                return null;
-            }
-
-            @Override
-            public Promise fold(Function function, Function function1) {
-                return null;
-            }
-
-            @Override
-            public void addListener(Runnable runnable, Executor executor) {
-
-            }
-
-            @Override
-            public boolean cancel(boolean mayInterruptIfRunning) {
-                return false;
-            }
-
-            @Override
-            public boolean isCancelled() {
-                return false;
-            }
-
-            @Override
-            public boolean isDone() {
-                return false;
-            }
-
-            @Override
-            public Object get() throws InterruptedException, ExecutionException {
-                return null;
-            }
-
-            @Override
-            public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-                return null;
-            }
-        };
-        PowerMockito.doReturn(p).when(mock,"getAndParse",Matchers.any(URI.class),Matchers.any(TestArrayJsonParser.class));
+        PowerMockito.doReturn(pr).when(mock,"getAndParse",Matchers.any(URI.class),Matchers.any(TestArrayJsonParser.class));
         Promise<Iterable<TestExecution.Test>> testResult= mock.getTests(testExecution);
-        Assert.assertNotNull(testResult);
+        Assert.assertNotNull(testResult.claim());
+    }
+
+    @Test
+    public void get() throws Exception {
+        testExecution = new TestExecution(URI.create(""), "EME-4644", 1977l);
+        TestExecution.Test test =  new TestExecution.Test(URI.create(""),"EA-3403",28775L,1,TestRun.Status.PASS);
+        try {
+            TestExecutionRestClientImpl mock = PowerMockito.spy(new TestExecutionRestClientImpl(URI.create(""),httpClient));
+            Promise<Iterable<TestExecution>> testResult= mock.get(test);
+            Assert.assertNotNull(testResult);
+        }catch (Exception e){
+
         }
+    }
+
 
 
 
--- collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testrun/TestRunRestClientImplTest.java
+++ collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testrun/TestRunRestClientImplTest.java
@@ -1,12 +1,9 @@
 package com.capitalone.dashboard.client.testrun;
 
 import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
-import com.atlassian.util.concurrent.Effect;
 import com.atlassian.util.concurrent.Promise;
 import com.capitalone.dashboard.client.api.domain.TestRun;
 import com.capitalone.dashboard.client.core.json.TestArrayJsonParser;
-import com.google.common.base.Function;
-import com.google.common.util.concurrent.FutureCallback;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,10 +16,6 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.net.URI;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(TestRunRestClientImpl.class)
@@ -40,77 +33,7 @@ public class TestRunRestClientImplTest {
     public final void init() throws Exception {
         MockitoAnnotations.initMocks(this);
         TestRun testRun = new TestRun(URI.create(""), "EME-1683", 507571L, TestRun.Status.PASS, null, null, "jqm884", "jqm884", null, null, null, null, null);
-        Promise pr =new Promise() {
-            @Override
-            public Object claim() {
-                return testRun;
-            }
-
-            @Override
-            public Promise done(Effect effect) {
-                return null;
-            }
-
-            @Override
-            public Promise fail(Effect effect) {
-                return null;
-            }
-
-            @Override
-            public Promise then(FutureCallback futureCallback) {
-                return null;
-            }
-
-            @Override
-            public Promise map(Function function) {
-                return null;
-            }
-
-            @Override
-            public Promise flatMap(Function function) {
-                return null;
-            }
-
-            @Override
-            public Promise recover(Function function) {
-                return null;
-            }
-
-            @Override
-            public Promise fold(Function function, Function function1) {
-                return null;
-            }
-
-            @Override
-            public void addListener(Runnable runnable, Executor executor) {
-
-            }
-
-            @Override
-            public boolean cancel(boolean mayInterruptIfRunning) {
-                return false;
-            }
-
-            @Override
-            public boolean isCancelled() {
-                return false;
-            }
-
-            @Override
-            public boolean isDone() {
-                return false;
-            }
-
-            @Override
-            public Object get() throws InterruptedException, ExecutionException {
-                return null;
-            }
-
-            @Override
-            public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-                return null;
-            }
-        };
+        PowerMockito.when(pr.claim()).thenReturn(testRun);
     }
     @Test
     public void getTestRunsByTestExecKeyAndTestKey() throws Exception {
@@ -118,7 +41,8 @@ public class TestRunRestClientImplTest {
         TestRunRestClientImpl mock = PowerMockito.spy(new TestRunRestClientImpl(URI.create(""),httpClient));
         PowerMockito.doReturn(pr).when(mock,"getAndParse",Matchers.anyObject(),Matchers.any(TestArrayJsonParser.class));
         Promise<TestRun> testruns = mock.getTestRun(TEST_EXEC_KEY, TEST_KEY);
-        Assert.assertNotNull(testruns);
+        Assert.assertNotNull(testruns.claim());
+        Assert.assertEquals("EME-1683", testruns.claim().getKey());
 
     }
     @Test
@@ -126,7 +50,8 @@ public class TestRunRestClientImplTest {
         TestRunRestClientImpl mock = PowerMockito.spy(new TestRunRestClientImpl(URI.create(""),httpClient));
         PowerMockito.doReturn(pr).when(mock,"getAndParse",Matchers.any(URI.class),Matchers.any(TestArrayJsonParser.class));
         Promise<TestRun> testruns = mock.getTestRun(TEST_ID);
-        Assert.assertNotNull(testruns);
+        Assert.assertNotNull(testruns.claim());
+        Assert.assertEquals("EME-1683", testruns.claim().getKey());
     }
 
 }
