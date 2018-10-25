4a8a67df9d42a26068dca330775dfb54777f5063
Praveen-chaganti <chagantipraveen5g3@gmail.com>
2018-10-25 14:52:43 -0400

    test_Execution test

--- collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testexecution/TestExecutionRestClientImplTest.java
+++ collectors/test-results/jira-xray/src/test/java/com/capitalone/dashboard/client/testexecution/TestExecutionRestClientImplTest.java
@@ -1,64 +1,122 @@
 package com.capitalone.dashboard.client.testexecution;
 
-import com.capitalone.dashboard.client.JiraXRayRestClientImpl;
-import com.capitalone.dashboard.client.JiraXRayRestClientSupplier;
-//import com.capitalone.dashboard.client.JiraXRayRestClientFactory;
+import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
+import com.atlassian.util.concurrent.Promise;
 import com.capitalone.dashboard.client.api.domain.TestExecution;
-import org.junit.After;
+import com.capitalone.dashboard.client.api.domain.TestRun;
+import com.capitalone.dashboard.client.core.json.TestArrayJsonParser;
+
+import com.capitalone.dashboard.model.Feature;
+import com.capitalone.dashboard.model.TestCapability;
+import com.capitalone.dashboard.model.TestCaseStatus;
+import com.capitalone.dashboard.repository.FeatureRepository;
+import com.capitalone.dashboard.repository.TestResultCollectorRepository;
+import com.capitalone.dashboard.repository.TestResultRepository;
+import com.capitalone.dashboard.util.FeatureCollectorConstants;
+import com.capitalone.dashboard.util.TestResultSettings;
+import org.bson.types.ObjectId;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TestExecutionRestClientImpl.class)
 public class TestExecutionRestClientImplTest {
 
-    private final String TEST_EXEC_KEY="EME-4944";
-    private final String TEST_KEY="EME-1946";
-    private final long TEST_ID=1977;
-
-    private final JiraXRayRestClientSupplier restClientSupplier=new JiraXRayRestClientSupplier();
-    private JiraXRayRestClientImpl restClient;
+    @Mock
     private TestExecution testExecution;
+    @Mock
+    private DisposableHttpClient httpClient;
+    @Mock
+    private Promise pr;
+    @Mock
+    FeatureRepository featureRepository;
+    @Mock
+    TestResultRepository testResultRepository;
+    @Mock
+    TestResultCollectorRepository testResultCollectorRepository;
+    TestResultSettings testResultSettings;
+
+
 
     @Before
-    public void setUp() throws Exception {
-        restClient= (JiraXRayRestClientImpl) restClientSupplier.get();
-        testExecution=new TestExecution(new URI(""),TEST_EXEC_KEY,1977l);
+    public final void init() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        testResultSettings.setPageSize(5);
+        TestExecution.Test test =  new TestExecution.Test(URI.create(""),"EA-3403",28775L,1,TestRun.Status.PASS);
+        PowerMockito.when(pr.claim()).thenReturn(test);
+        ObjectId objectId = new ObjectId("5af11dd28902ccb2d87fcdab");
+        //PowerMockito.when(testResultCollectorRepository.findByName(FeatureCollectorConstants.JIRA_XRAY).getId()).thenReturn(objectId);
+
+
 
     }
-
-    @After
-    public void tearDown() throws Exception {
-
+    @Test
+    public void getTests() throws Exception{
+        testExecution = new TestExecution(URI.create(""), "EME-4644", 1977l);
+        TestExecutionRestClientImpl mock = PowerMockito.spy(new TestExecutionRestClientImpl(URI.create(""),httpClient,testResultCollectorRepository,testResultRepository,featureRepository));
+        PowerMockito.doReturn(pr).when(mock,"getAndParse",Matchers.any(URI.class),Matchers.any(TestArrayJsonParser.class));
+        Promise<Iterable<TestExecution.Test>> testResult= mock.getTests(testExecution);
+        Assert.assertNotNull(testResult.claim());
+        System.out.println(testResult.claim());
     }
 
     @Test
-    public void testGetTests() throws Exception {
-        Iterable<TestExecution.Test>tests= restClient.getTestExecutionClient().getTests(testExecution).claim();
-        restClient.getTestExecutionClient().setTests(testExecution);
+    public void get() throws Exception {
+        testExecution = new TestExecution(URI.create(""), "EME-4644", 1977l);
+        TestExecution.Test test =  new TestExecution.Test(URI.create(""),"EA-3403",28775L,1,TestRun.Status.PASS);
+        try {
+            TestExecutionRestClientImpl mock = PowerMockito.spy(new TestExecutionRestClientImpl(URI.create(""),httpClient,testResultCollectorRepository,testResultRepository,featureRepository));
+            Promise<Iterable<TestExecution>> testResult= mock.get(test);
+            Assert.assertNotNull(testResult);
+        }catch (Exception e){
 
-        for(TestExecution.Test t:tests)
-        {
-            System.out.print("\n TEST KEY: " + t);
-            System.out.print("\n TEST Version: " + t.getVersion());
-            assertNotNull(t);
         }
     }
-//
-//    @Test
-//    public void testSetTests() throws Exception {
-//
-//    }
-//
-//    @Test
-//    public void testRemoveTest() throws Exception {
-//
-//    }
-//
-//    @Test
-//    public void testGet() throws Exception {
-//
-//    }
+
+    @Test
+    public void updateMongoTest_ExecInformation(){
+        TestExecutionRestClientImpl mock = PowerMockito.spy(new TestExecutionRestClientImpl(URI.create(""),httpClient,testResultCollectorRepository,testResultRepository,featureRepository));
+        int count = mock.updateTestExecutionInformation();
+        PowerMockito.when(featureRepository.getStoryByTeamID("503")).thenReturn(createFeature());
+       // PowerMockito.when(mock.)
+    }
+
+    private List<Feature> createFeature() {
+        List<Feature> features = new ArrayList<>();
+        Feature feature1 = new Feature();
+        feature1.setsTeamID("503");
+        feature1.setsName("summary1001");
+        feature1.setsProjectName("Hygieia");
+        features.add(feature1);
+        return features;
+    }
+
+
+    private List<TestCapability> createTestCapability(Iterable<TestExecution.Test> test,Feature feature){
+        List<TestCapability> testCapabilities = new ArrayList<>();
+        TestCapability testCapability = new TestCapability();
+        testCapability.setDescription("summary1001");
+        testCapability.setTotalTestSuiteCount(1);
+        testCapability.setStatus(TestCaseStatus.Success);
+        //testCapability.setTestSuites(this.c);
+        testCapabilities.add(testCapability);
+        return testCapabilities;
+    }
+
+
+
+
+
+
+
 }
